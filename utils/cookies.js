513cf5ca7046b7d62935d726d9244cdd5fce6ce7
Kemal Demir Sen <kemaldemirsen@Kemal-MacBook-Air.local>
2024-09-18 17:15:20 +0400

    update

--- utils/cookies.js
+++
@@ -1,8 +0,0 @@
-const { parse } = require('cookie');
-
-function getCookie(cookieHeader, name) {
-  const cookies = parse(cookieHeader || '');
-  return cookies[name];
-}
-
-module.exports = { getCookie };
